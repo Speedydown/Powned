4632b12b6e5c2eaacf194f1802d556ced3129306
Teake Otter <teake@wiezitwaarvandaag.nl>
2016-02-12 15:16:40 +0100

    update

--- PownedLogic/Models/Headline.cs
+++ PownedLogic/Models/Headline.cs
@@ -50,7 +50,7 @@ namespace PownedLogic.Model
                     if (Window.Current.Bounds.Width > Window.Current.Bounds.Height)
                     {
                         //Landscape
-                        return ((Bounds - 110) / 3) - 10;
+                        return ((Bounds - 150) / 3) - 10;
                     }
                     else
                     {
